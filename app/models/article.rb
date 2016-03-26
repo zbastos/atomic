0fc8444643b378e6fd296488d40d22681c2a31f0
Fernando Mendes <devfrmendes@gmail.com>
2016-03-26 12:40:02 +0000

    Adds image to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
