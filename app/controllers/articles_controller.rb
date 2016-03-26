2b83ea2c4f407c80f4f5a0ebdf994d9ed1cc20cf
Fernando Mendes <devfrmendes@gmail.com>
2016-03-26 13:33:55 +0000

    Removes show action from articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,8 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :set_article, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.all
-  end
-
-  def show
   end
 
   def new
@@ -21,7 +18,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to @article
+      redirect_to action: 'index'
     else
       render :new
     end
@@ -29,7 +26,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      redirect_to @article
+      redirect_to action: 'index'
     else
       render :edit
     end
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
     resources :editions, only: [:index, :new, :create, :destroy]
   end
 
-  resources :articles, path: 'news'
+  resources :articles, path: 'news', except: [:show]
 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
