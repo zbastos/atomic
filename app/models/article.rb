c105e44d5291b73c3713561d1235217ce4f361ed
Fernando Mendes <devfrmendes@gmail.com>
2016-03-26 12:42:40 +0000

    Adds missing article validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ActiveRecord::Base
   belongs_to :user
 
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :body,  presence: true
+
   has_attached_file :image, styles: { large: "600x600>", medium: "300x300>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
