74854fc6b72cf6a108f4097feeebe85180d0ed11
0urobor0s <lhome.gamer@gmail.com>
2017-11-03 21:40:26 +0000

    Remove unnecessary comments

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,25 +1,20 @@
 class PartnersController < ApplicationController
   before_action :set_partner, only: [:show, :edit, :update, :destroy]
 
-  # GET /partners
   def index
     @partners = Partner.all
   end
 
-  # GET /partners/1
   def show
   end
 
-  # GET /partners/new
   def new
     @partner = Partner.new
   end
 
-  # GET /partners/1/edit
   def edit
   end
 
-  # POST /partners
   def create
     @partner = Partner.new(partner_params)
 
@@ -30,7 +25,6 @@ class PartnersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /partners/1
   def update
     if @partner.update(partner_params)
       redirect_to @partner, notice: 'Partner was successfully updated.'
@@ -39,19 +33,16 @@ class PartnersController < ApplicationController
     end
   end
 
-  # DELETE /partners/1
   def destroy
     @partner.destroy
     redirect_to partners_url, notice: 'Partner was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_partner
       @partner = Partner.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def partner_params
       params.require(:partner).permit(:name, :benefits, :logo)
     end
